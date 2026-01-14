4a6abbbc1ecd9cb54d0b5c94314fb43bace6d73b
rajpatil197 <patilrajvardhan420@gmail.com>
2026-01-14 16:00:16 +0530

    Add Prescriptions table

---
+++ Database/OPD.sql
@@ -0,0 +1,25 @@
+create database abhijit1_mygp;
+
+use abhijit1_mygp;
+select * from patients;
+
+create database opd_management;
+
+use opd_management;
+select * from doctors;
+SELECT role FROM doctors WHERE email='siddhi02@gmail.com';
+
+select * from patients;
+select * from visits;
+select * from diagnostics;
+select * from medicines;
+select * from referralcenters;
+select * from testmasters;
+
+select * from referrals;
+select * from bills;
+select * from visitreports;
+
+select * from pathologytest;
+
+select * from receptions;
